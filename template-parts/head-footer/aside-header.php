ab15d5acfbb2bf98ef2c36ab8594167ce136bd21
Andrey <84500846+AndrejjBel@users.noreply.github.com>
2024-06-26 09:43:55 +0300

    5
    Edit

--- template-parts/head-footer/aside-header.php
+++ template-parts/head-footer/aside-header.php
@@ -17,15 +17,6 @@
         </button>
     </div>
     <div class="sidebar-nav__content">
-        <div class="sidebar-nav__content__profile">
-            <?php if ( function_exists( 'glamping_club_user_dname_sidebar' ) ) glamping_club_user_dname_sidebar(); ?>
-            <!-- <a href="#" class="sidebar-avatar"><img src="images/avatar/01.jpg" alt="avatar"></a>
-            <h4><a href="#" class="sidebar-name">Jackon Honson</a></h4>
-            <a href="ad-post.html" class="btn btn-inline sidebar-post">
-                <i class="fas fa-plus-circle"></i>
-                <span>post your ad</span>
-            </a> -->
-        </div>
         <div class="sidebar-nav__content__menu">
             <?php
             wp_nav_menu( array(
@@ -34,102 +25,10 @@
             ) );
             if ( function_exists( 'glamping_club_menu_admin' ) ) glamping_club_menu_admin();
             ?>
-            <!-- <ul class="nav nav-tabs">
-                <li><a href="#main-menu" class="nav-link active" data-toggle="tab">Main Menu</a></li>
-                <li><a href="#author-menu" class="nav-link" data-toggle="tab">Author Menu</a></li>
-            </ul>
-
-            <div class="tab-pane active" id="main-menu">
-                <ul class="navbar-list">
-                    <li class="navbar-item"><a class="navbar-link" href="index.html">Home</a></li>
-                    <li class="navbar-item navbar-dropdown">
-                        <a class="navbar-link" href="#">
-                            <span>Categories</span>
-                            <i class="fas fa-plus"></i>
-                        </a>
-                        <ul class="dropdown-list">
-                            <li><a class="dropdown-link" href="category-list.html">category list</a></li>
-                            <li><a class="dropdown-link" href="category-details.html">category details</a></li>
-                        </ul>
-                    </li>
-                    <li class="navbar-item navbar-dropdown">
-                        <a class="navbar-link" href="#">
-                            <span>Advertise List</span>
-                            <i class="fas fa-plus"></i>
-                        </a>
-                        <ul class="dropdown-list">
-                            <li><a class="dropdown-link" href="ad-list-column3.html">ad list column 3</a></li>
-                            <li><a class="dropdown-link" href="ad-list-column2.html">ad list column 2</a></li>
-                            <li><a class="dropdown-link" href="ad-list-column1.html">ad list column 1</a></li>
-                        </ul>
-                    </li>
-                    <li class="navbar-item navbar-dropdown">
-                        <a class="navbar-link" href="#">
-                            <span>Advertise details</span>
-                            <i class="fas fa-plus"></i>
-                        </a>
-                        <ul class="dropdown-list">
-                            <li><a class="dropdown-link" href="ad-details-grid.html">ad details grid</a></li>
-                            <li><a class="dropdown-link" href="ad-details-left.html">ad details left</a></li>
-                            <li><a class="dropdown-link" href="ad-details-right.html">ad details right</a></li>
-                        </ul>
-                    </li>
-                    <li class="navbar-item navbar-dropdown">
-                        <a class="navbar-link" href="#">
-                            <span>Pages</span>
-                            <i class="fas fa-plus"></i>
-                        </a>
-                        <ul class="dropdown-list">
-                            <li><a class="dropdown-link" href="about.html">About Us</a></li>
-                            <li><a class="dropdown-link" href="compare.html">Ad Compare</a></li>
-                            <li><a class="dropdown-link" href="cities.html">Ad by Cities</a></li>
-                            <li><a class="dropdown-link" href="price.html">Pricing Plan</a></li>
-                            <li><a class="dropdown-link" href="user-form.html">User Form</a></li>
-                            <li><a class="dropdown-link" href="404.html">404</a></li>
-                        </ul>
-                    </li>
-                    <li class="navbar-item navbar-dropdown">
-                        <a class="navbar-link" href="#">
-                            <span>blogs</span>
-                            <i class="fas fa-plus"></i>
-                        </a>
-                        <ul class="dropdown-list">
-                            <li><a class="dropdown-link" href="blog-list.html">Blog list</a></li>
-                            <li><a class="dropdown-link" href="blog-details.html">blog details</a></li>
-                        </ul>
-                    </li>
-                    <li class="navbar-item"><a class="navbar-link" href="contact.html">Contact</a></li>
-                </ul>
-            </div>
-
-            <div class="tab-pane" id="author-menu">
-                <ul class="navbar-list">
-                    <li class="navbar-item"><a class="navbar-link" href="dashboard.html">Dashboard</a></li>
-                    <li class="navbar-item"><a class="navbar-link" href="profile.html">Profile</a></li>
-                    <li class="navbar-item"><a class="navbar-link" href="ad-post.html">Ad Post</a></li>
-                    <li class="navbar-item"><a class="navbar-link" href="my-ads.html">My Ads</a></li>
-                    <li class="navbar-item"><a class="navbar-link" href="setting.html">Settings</a></li>
-                    <li class="navbar-item navbar-dropdown">
-                        <a class="navbar-link" href="bookmark.html">
-                            <span>bookmark</span>
-                            <span>0</span>
-                        </a>
-                    </li>
-                    <li class="navbar-item navbar-dropdown">
-                        <a class="navbar-link" href="message.html">
-                            <span>Message</span>
-                            <span>0</span>
-                        </a>
-                    </li>
-                    <li class="navbar-item navbar-dropdown">
-                        <a class="navbar-link" href="notification.html">
-                            <span>Notification</span>
-                            <span>0</span>
-                        </a>
-                    </li>
-                    <li class="navbar-item"><a class="navbar-link" href="user-form.html">Logout</a></li>
-                </ul>
-            </div> -->
+        </div>
+        
+        <div class="sidebar-nav__content__profile">
+            <?php if ( function_exists( 'glamping_club_user_dname_sidebar' ) ) glamping_club_user_dname_sidebar(); ?>
         </div>
 
         <div class="sidebar-nav__content__footer">
