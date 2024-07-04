51fdce82ab497c9a10301890d19482e8968444e5
Andrey <84500846+AndrejjBel@users.noreply.github.com>
2024-07-04 09:21:36 +0300

    1
    Edit

--- template-parts/head-footer/footer.php
+++ template-parts/head-footer/footer.php
@@ -25,7 +25,7 @@ $site_options = get_option( 'glc_options' );
             <div class="footer-generale__middle">
                 <div class="footer-generale__middle__item">
                     <div class="footer-generale__middle__item__title">
-                        <h3><?php echo $site_options['contact_title']; ?></h3>
+                        <h3><?php echo ($site_options)? $site_options['contact_title'] : ''; ?></h3>
                     </div>
                     <div class="footer-generale__middle__item__content">
                         <ul>
@@ -33,7 +33,7 @@ $site_options = get_option( 'glc_options' );
                                 <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
                                     <path d="M168.3 499.2C116.1 435 0 279.4 0 192C0 85.96 85.96 0 192 0C298 0 384 85.96 384 192C384 279.4 267 435 215.7 499.2C203.4 514.5 180.6 514.5 168.3 499.2H168.3zM192 256C227.3 256 256 227.3 256 192C256 156.7 227.3 128 192 128C156.7 128 128 156.7 128 192C128 227.3 156.7 256 192 256z"/>
                                 </svg>
-                                <span><?php echo $site_options['address']; ?></span>
+                                <span><?php echo ($site_options)? $site_options['address'] : ''; ?></span>
                             </li>
                             <li class="contact-item">
                                 <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
