10a470f0669b2ff3a24bfca34121869da88f8d73
Andrey <84500846+AndrejjBel@users.noreply.github.com>
2024-08-01 20:13:01 +0300

    8
    Edit

--- functions/account/templates/tabs/tab-glemp.php
+++ functions/account/templates/tabs/tab-glemp.php
@@ -2,4 +2,10 @@
     <div class="dashboard-tab__title">
         <?php echo __( 'Глэмпинги', 'glamping-club' ); ?>
     </div>
+
+    <div class="dashboard-tab__content">
+        <div class="dashboard-tab__content__glamps user-glamps">
+            <?php dashboard_glamp_user(); ?>
+        </div>
+    </div>
 </div>
