f2d9b965dca18e185a67e453cb29b55081247f2d
Andrey <84500846+AndrejjBel@users.noreply.github.com>
2024-08-22 10:48:51 +0300

    1
    Edit

--- template-parts/glampings/single.php
+++ template-parts/glampings/single.php
@@ -147,11 +147,11 @@ $cur_user_id = get_current_user_id();
                     data-services="vkontakte,odnoklassniki" title="Поделиться"></div>
 
                 <?php if ($cur_user_id) { ?>
-                    <button id="add-claim" data-postid="<?php the_ID(); ?>" data-userid="<?php echo $cur_user_id; ?>" class="round-sup-red" type="button" name="button" title="Это мой глэмпинг">
+                    <!-- <button id="add-claim" data-postid="<?php //the_ID(); ?>" data-userid="<?php //echo $cur_user_id; ?>" class="round-sup-red" type="button" name="button" title="Это мой глэмпинг">
                         <svg class="add-claim" width="40" height="40" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                             <path d="M256 0C114.6 0 0 114.6 0 256s114.6 256 256 256s256-114.6 256-256S397.4 0 256 0zM256 464c-114.7 0-208-93.31-208-208S141.3 48 256 48s208 93.31 208 208S370.7 464 256 464zM256 304c13.25 0 24-10.75 24-24v-128C280 138.8 269.3 128 256 128S232 138.8 232 152v128C232 293.3 242.8 304 256 304zM256 337.1c-17.36 0-31.44 14.08-31.44 31.44C224.6 385.9 238.6 400 256 400s31.44-14.08 31.44-31.44C287.4 351.2 273.4 337.1 256 337.1z"/>
                         </svg>
-                    </button>
+                    </button> -->
                 <?php } ?>
             </div>
         </div>
