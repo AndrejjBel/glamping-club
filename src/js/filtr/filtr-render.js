88fe38882aa9e3286f77e77b646f9fb832532968
Andrey <84500846+AndrejjBel@users.noreply.github.com>
2025-02-04 10:29:37 +0300

    3
    Edit

--- src/js/filtr/filtr-render.js
+++ src/js/filtr/filtr-render.js
@@ -180,10 +180,14 @@ window.addEventListener('resize', () => {
             let pages = Math.ceil(pagesAbs);
             const navLinks = document.querySelector('.filtr-pagination .nav-links');
             if (pages > 1) {
-                document.querySelector('.pagination').style.display = 'none';
+                if (document.querySelector('.pagination')) {
+                    document.querySelector('.pagination').style.display = 'none';
+                }
                 document.querySelector('.filtr-pagination').style.display = 'flex';
             } else {
-                document.querySelector('.pagination').style.display = 'none';
+                if (document.querySelector('.pagination')) {
+                    document.querySelector('.pagination').style.display = 'none';
+                }
                 document.querySelector('.filtr-pagination').style.display = '';
             }
 
@@ -202,10 +206,14 @@ window.addEventListener('resize', () => {
             let pages = Math.ceil(pagesAbs);
             const navLinks = document.querySelector('.filtr-pagination .nav-links');
             if (pages > 1) {
-                document.querySelector('.pagination').style.display = 'none';
+                if (document.querySelector('.pagination')) {
+                    document.querySelector('.pagination').style.display = 'none';
+                }
                 document.querySelector('.filtr-pagination').style.display = 'flex';
             } else {
-                document.querySelector('.pagination').style.display = 'none';
+                if (document.querySelector('.pagination')) {
+                    document.querySelector('.pagination').style.display = 'none';
+                }
                 document.querySelector('.filtr-pagination').style.display = '';
             }
 
